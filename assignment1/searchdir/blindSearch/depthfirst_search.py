4b68431b7cd59e81c505c15e5c50e7e9fa47100d
AG3X29M4Nc5DJN0-FNkr5MSgiwR4YxBz <discipla@paradise.lan>
2017-09-30 13:14:03 +0000

    Depth first search - implemented need to test once node is finished

--- assignment1/searchdir/blindSearch/depthfirst_search.py
+++ assignment1/searchdir/blindSearch/depthfirst_search.py
@@ -5,3 +5,17 @@ from searchdir.util import *
 ## It must return the solution node and the number of visited nodes
 def depthfirst_search(initialState):
     print('DFS ----------------------------------')
+    fringe = Stack()
+    fringe.push(initialState)
+    closed = []
+    while(not fringe.isEmpty()):
+        currentNode = fringe.pop()
+        #TODO what is solution node : to implement?
+        if (currentNode == solutionNode):
+            return currentNode
+        #TODO modify to fit the nodeclass
+        children = currentNode.generate
+        closed.append(currentNode)
+        for node in children:
+            if(node not in closed and node not in fringe):
+                fringe.push(node)
