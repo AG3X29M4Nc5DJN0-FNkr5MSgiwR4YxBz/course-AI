da35d90b27e1069b8e21c4da2135f1fc20338bb6
aboye018@uottawa.ca <aboye018@uottawa.ca>
2017-10-08 13:51:38 -0400

    Started debugging depth first search. Algo stays stuck on infinite child developement loop

--- assignment1/searchdir/blindSearch/depthfirst_search.py
+++ assignment1/searchdir/blindSearch/depthfirst_search.py
@@ -11,19 +11,19 @@ def depthfirst_search(initialState):
     node = Node(initialState)
     fringe.push(node)
     closed = []
+    nb_visited = 0
     while(not fringe.isEmpty()):
         currentNode = fringe.pop()
         if (currentNode.state.isGoal()):
-            return currentNode
+            return currentNode, nb_visited
         listActions = currentNode.state.possibleActions()
+        nb_visited += 1
         children = []
         #Create a new node, make the action and add the children list
         for a in listActions:
             #Copy node, make action then store in stack
-            childNode = EightPuzzleState(currentNode.configuration)
-            childNode.executeAction(a)
+            childNode = Node(currentNode.state.executeAction(a))
             children.append(childNode)
-        
         closed.append(currentNode)
         for node in children:
             if(node not in closed and node not in fringe):
