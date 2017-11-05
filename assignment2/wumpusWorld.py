929102023af6b6433f58ef1b0f41a25ef13c44a8
AG3X29M4Nc5DJN0-FNkr5MSgiwR4YxBz <discipla@paradise.lan>
2017-11-05 20:37:23 +0000

    Completed part 2

--- assignment2/wumpusWorld.py
+++ assignment2/wumpusWorld.py
@@ -37,11 +37,37 @@ class wumpusWorld():
         self.room[0][0] = 0
 
         #Initial agent configuration
-        aPos = [0,0]
-        aDir = 'r'
+        self.aPos = [0,0]
+        self.aDir = 'r'
 
-        #Sensation in each rooms
-        #TODO
+        self.sensation = self.getSensation(0,0)
+        print(self.sensation)
+
+    #Sensation felt by the agent
+    #[Stench,Breeze,Glitter,Bump,Scream]
+    #Return a sensation array for the room at x,y
+    def getSensation(self,x,y):
+        sensation = [0,0,0,0,0]
+        #Determine directly adjacent room
+        roomList = []
+        if(x > 0):
+            roomList.append(self.room[x-1][y])
+        if(x < 3):
+            roomList.append(self.room[x+1][y])
+        if(y > 0):
+            roomList.append(self.room[x][y-1])
+        if(y < 3):
+            roomList.append(self.room[x][y+1])
+        for r in roomList:
+            #Stench
+            if(r == 'W'):
+                sensation[0] = "Stench"
+            elif(r == 'P'):
+                sensation[1] = "Breeze"
+        if(self.room[x][y] == 'G'):
+            sensation[2] = "Glitter"
+        return sensation
+
 
     def printRoom(self):
         for j in range(3,-1,-1):
