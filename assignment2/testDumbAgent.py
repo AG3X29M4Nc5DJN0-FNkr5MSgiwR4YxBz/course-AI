4b12e4d281676d1af58f763c28d5f0b17b3a471b
AG3X29M4Nc5DJN0-FNkr5MSgiwR4YxBz <discipla@paradise.lan>
2017-11-20 21:52:00 +0000

    Added code to test dumbAgent

---
+++ assignment2/testDumbAgent.py
@@ -0,0 +1,25 @@
+from wumpusWorld import *
+from agent import *
+from knowledgeAgent import *
+
+w0 = wumpusWorld()
+#Set the world
+for i in range(0,4):
+    for j in range(0,4):
+        w0.r[i][j] = room()
+
+w0.r[2][0].pit = True
+w0.r[0][2].wumpus = True
+w0.r[1][2].gold = True
+w0.r[2][2].pit = True
+w0.r[3][3].pit = True
+w0.r[0][0].agent = True
+
+#Create agent
+print("Creating Agent")
+a0 = agent(0,0,w0)
+while(not a0.terminated):
+    w0.printRoom()
+    a0.dumbAgent(w0)
+    a0.status()
+
